ae01c0f69e060bbb29a643ad3e904865d0c7ba05
Sam Fuksa <sam@samfuksa.eu>
2025-05-23 08:58:46 +0200

    zmena vysky

--- atrakce.cmd
+++ atrakce.cmd
@@ -2,9 +2,9 @@
 
 set /p vyska=Zadej vysku:
 
-set minVyska=150
+set minVyska=140
 
-if %vyska% GTR %minVyska% (echo Mas dostatecnou vysku, uzij si tobogan
+if %vyska% GTR %minVyska% (echo Mas dostatecnou vysku, uzij si xd tobogan
 ) else (echo Nemas dostatecnou vysku, utikej do brouzdaliste)
 
 
