47d2f5934bfe3623540acf2b804e72faf6d01806
Sam Fuksa <sam@samfuksa.eu>
2025-05-23 09:30:20 +0200

    bmi kalkulacka prvni verze

---
+++ bmi.bat
@@ -0,0 +1,14 @@
+@echo off
+
+
+REM nacitani promenych
+set /p vyska=zadej vysku v metrech:
+set /p vaha=zadej vahu v kg:
+
+
+set vypocet=%vaha% / %vyska% * %vyska%
+
+echo BMI pro vysku %vyska% a vahu %vaha% je: %vypocet%
+
+
+pause
